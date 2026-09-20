c5f50de7617c08839d66c9a370580697a0951611
wnakamizo <w.nakamizo@genbasupport.com>
2026-09-20 15:27:33 +0900

    Use class << self instead of repeating self. in MemoRepository

--- memo_repository.rb
+++ memo_repository.rb
@@ -3,35 +3,37 @@
 require 'pg'
 
 module MemoRepository
-  def self.connection
-    @connection ||= PG.connect(dbname: 'memos')
-  end
+  class << self
+    def connection
+      @connection ||= PG.connect(dbname: 'memos')
+    end
 
-  def self.load_memos
-    connection.exec('SELECT * FROM memos ORDER BY id')
-  end
+    def load_memos
+      connection.exec('SELECT * FROM memos ORDER BY id')
+    end
 
-  def self.find(id)
-    connection.exec_params('SELECT * FROM memos WHERE id = $1', [id])
-  end
+    def find(id)
+      connection.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+    end
 
-  def self.create(memo_params)
-    result = connection.exec_params(
-      'INSERT INTO memos (title, description) VALUES ($1, $2) RETURNING id',
-      [memo_params[:title], memo_params[:description]]
-    )
-    result[0]['id']
-  end
+    def create(memo_params)
+      result = connection.exec_params(
+        'INSERT INTO memos (title, description) VALUES ($1, $2) RETURNING id',
+        [memo_params[:title], memo_params[:description]]
+      )
+      result[0]['id']
+    end
 
-  def self.edit(id, memo_params)
-    result = connection.exec_params(
-      'UPDATE memos SET title = $1, description = $2 WHERE id = $3',
-      [memo_params[:title], memo_params[:description], id]
-    )
-    result.cmd_tuples.positive?
-  end
+    def edit(id, memo_params)
+      result = connection.exec_params(
+        'UPDATE memos SET title = $1, description = $2 WHERE id = $3',
+        [memo_params[:title], memo_params[:description], id]
+      )
+      result.cmd_tuples.positive?
+    end
 
-  def self.delete(id)
-    connection.exec_params('DELETE FROM memos WHERE id = $1', [id])
+    def delete(id)
+      connection.exec_params('DELETE FROM memos WHERE id = $1', [id])
+    end
   end
 end
